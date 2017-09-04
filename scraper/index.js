3a3164df7fcfe3e504e3d4d305a1e35b89561a09
ankihg <hagelin.annika@gmail.com>
2017-09-04 11:38:52 -0700

    made async

--- scraper/index.js
+++ scraper/index.js
@@ -2,6 +2,7 @@ const request = require('request');
 const cheerio = require('cheerio');
 const async = require('async');
 
+const host = 'https://www.azlyrics.com';
 const url = 'https://www.azlyrics.com/a/arcadefire.html';
 
 request(url, (err, response, html) => {
@@ -10,19 +11,34 @@ request(url, (err, response, html) => {
     let $ = cheerio.load(html);
     let albums = {};
     let activeAlbum = null;
-    $('#listAlbum').children().filter(function() {
-        var el = $(this);
+    let children = $('#listAlbum').children();
+    async.each(children,
+        function(child) {
+            var el = $(child);
 
-        if (el[0].name == 'div') {
-            activeAlbum = el.text();
-            albums[el.text()] = [];
-        } else if (el[0].name == 'a') {
-            if (activeAlbum)
-                albums[activeAlbum].push({
-                    name: el.text(),
-                    lyricLink: el.attr('href'),
-                });
-        }
-    });
+            if (el[0].name == 'div') {
+                activeAlbum = el.text();
+                albums[el.text()] = [];
+            } else if (el[0].name == 'a') {
+                if (!el.text()) return;
+                let lyricsLink = el.attr('href');
+
+                albums[activeAlbum].push(el.text());
+
+            }
+        }, (e) => {
+            console.log(e || 'success');
+        });
+
+    // $('#listAlbum').children().filter(function() {
+    //
+    // });
     console.log(albums);
 });
+
+
+function scrapeLyrics(url) {
+    request(url, (err, response, html) => {
+
+    });
+}
