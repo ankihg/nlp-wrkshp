ec2eefc645e4ea150a5c1c86d7fc1e30f7cd9b31
ankihg <hagelin.annika@gmail.com>
2017-09-04 11:10:46 -0700

    parse album hash

--- scraper/index.js
+++ scraper/index.js
@@ -7,13 +7,22 @@ request(url, (err, response, html) => {
     if (err) console.log('error', err);
 
     let $ = cheerio.load(html);
+    let albums = {};
+    let activeAlbum = null;
     $('#listAlbum').children().filter(function() {
         var el = $(this);
         // console.log('show el', el[0]);
         // console.log('show el', el[0].name);
-        if (el[0].name == 'div')
-            console.log('album', el.text());
-        else if (el[0].name == 'a')
-            console.log('song', el.text());
+        if (el[0].name == 'div') {
+            // console.log('album', el.text());
+            activeAlbum = el.text();
+            albums[el.text()] = [];
+        }
+        else if (el[0].name == 'a') {
+            // console.log('song', el.text());
+            if (activeAlbum)
+                albums[activeAlbum].push(el.text());
+        }
     });
+    console.log(albums);
 });
