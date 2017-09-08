3845047bca6f566346a342d7e893125f39d79120
ankihg <hagelin.annika@gmail.com>
2017-09-07 20:35:37 -0700

    a partially successful full scrape

--- scraper/index.js
+++ scraper/index.js
@@ -5,51 +5,55 @@ const async = require('async');
 const host = 'https://www.azlyrics.com';
 const url = 'https://www.azlyrics.com/a/arcadefire.html';
 
-// request(url, (err, response, html) => {
-//     if (err) console.log('error', err);
-//
-//     let $ = cheerio.load(html);
-//     let albums = {};
-//     let activeAlbum = null;
-//     let children = $('#listAlbum').children();
-//     async.each(children,
-//         function(child, go) {
-//             let el = $(child);
-//
-//             if (el[0].name == 'div') {
-//                 activeAlbum = el.text();
-//                 albums[el.text()] = [];
-//                 return go();
-//             } if (el[0].name == 'a') {
-//                 if (!el.text()) return;
-//                 let lyricsLink = el.attr('href');
-//                 return scrapeLyrics(host + lyricsLink.slice(2), (e) => {
-//                     return go(e);
-//                 });
-//             }
-//         }, (e) => {
-//             console.log(e || 'success');
-//             console.log(albums);
-//         });
-// });
+request(url, (err, response, html) => {
+    if (err) console.log('error', err);
 
-let lyricsUrl = 'https://www.azlyrics.com/lyrics/arcadefire/everythingnow.html';
+    let $ = cheerio.load(html);
+    let albums = {};
+    let activeAlbum = null;
+    let children = $('#listAlbum').children();
+    async.each(children,
+        function(child, go) {
+            let el = $(child);
+
+            if (el[0].name == 'div') {
+                activeAlbum = el.text();
+                albums[el.text()] = [];
+                return go();
+            } if (el[0].name == 'a') {
+                if (!el.text()) return go();
+                let lyricsLink = el.attr('href');
+                let song = {
+                    name: el.text(),
+                    lyricsLink: el.attr('href')
+                };
+                albums[activeAlbum].push(song);
+                return scrapeLyrics(host + lyricsLink.slice(2), (e, lyricsText) => {
+                    if (e) return go(e);
+                    song.lyrics = lyricsText;
+                    return go();
+                });
+            }
+            return go();
+        }, (e) => {
+            console.log(e || 'success');
+            console.log(JSON.stringify(albums, null, 4));
+        });
+});
+
 function scrapeLyrics(url, go) {
+    console.log('scraping', url);
     request(url, (err, response, html) => {
         let $ = cheerio.load(html);
         // let divs = $('div:not([class])')
         let mainPage = $('.main-page');
         let row = mainPage.children('.row');
         let lyricsDiv = row.find('div:not([class])');
-        console.log(lyricsDiv.text());
-        // divs.filter(() => {
-        //     let div = $(this);
-        //     console.log(div);
-        //     console.log(div.text());
-        // });
-        return go();
+        // console.log(lyricsDiv.text());
+        return go(null, lyricsDiv.text());
     });
 }
-scrapeLyrics(lyricsUrl, () => {
-    console.log('hii');
-})
+// let lyricsUrl = 'https://www.azlyrics.com/lyrics/arcadefire/everythingnow.html';
+// scrapeLyrics(lyricsUrl, () => {
+//     console.log('hii');
+// })
