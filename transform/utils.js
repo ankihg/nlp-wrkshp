7fa69472162ed0cf8c76e3caa9abbecfd166138b
ankihg <hagelin.annika@gmail.com>
2018-01-21 18:17:54 -0800

    built wordToThemes

--- transform/utils.js
+++ transform/utils.js
@@ -1,13 +1,13 @@
 const natural = require('natural');
-
-const metadata = require('./metadata');
-
+const stem = natural.PorterStemmer.stem;
+const wordToThemes = _buildWordToThemes(require('./metadata').themes);
+console.log('wordToThemes', wordToThemes);
 module.exports = {
     matchThemes(holder, occurrence, piece) {
-        let word = natural.PorterStemmer.stem(occurrence.word.toLowerCase());
-        let themes = metadata.wordToThemes[word] || [];
+        let word = stem(occurrence.word.toLowerCase());
+        let themes = wordToThemes[word] || [];
         // if (metadata.themes[word]) themes.push(word);
-        themes.forEach((theme) => {
+        Object.keys(themes).forEach((theme) => {
             holder[theme] = holder[theme] || {};
             holder[theme][word] = holder[theme][word] || [];
             holder[theme][word].push(occurrence.line);
@@ -21,5 +21,13 @@ module.exports = {
 
 
 function _buildWordToThemes(themes) {
-
+    return Object.keys(themes).reduce((wordToThemes, themeKey) => {
+        wordToThemes[stem(themeKey)] = wordToThemes[stem(themeKey)] || {};
+        wordToThemes[stem(themeKey)][themeKey] = true;
+        Object.keys(themes[themeKey]).forEach((word) => {
+            wordToThemes[stem(word)] = wordToThemes[stem(word)] || {};
+            wordToThemes[stem(word)][themeKey] = true;
+        });
+        return wordToThemes;
+    }, {});
 }
